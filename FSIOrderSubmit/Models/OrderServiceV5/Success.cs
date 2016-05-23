d61e5cdfa1631375229b9b9b767513744294688f
Alston Callwood <alston80@gmail.com>
2016-05-23 15:25:08 -0400

    setting things up

---
+++ FSIOrderSubmit/Models/OrderServiceV5/Success.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.OrderServiceV5
+{
+    public class Success
+    {
+        public Guid OrderId { get; set; }
+    }
+}
--- FSIOrderSubmit/Properties/Settings.Designer.cs
+++ FSIOrderSubmit/Properties/Settings.Designer.cs
@@ -8,22 +8,84 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FSIOrderSubmit.Properties
-{
-
-
+namespace FSIOrderSubmit.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://test.fsicatalog.com")]
+        public string TestOrderServiceBaseAddress {
+            get {
+                return ((string)(this["TestOrderServiceBaseAddress"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("S0s6QTYwRkFERDQtNTZDNi00N0ExLTkzMTYtNjUzOUQwNTk2RDRE")]
+        public string TestOrderServiceApiKey {
+            get {
+                return ((string)(this["TestOrderServiceApiKey"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://orders.fsifulfillment.com")]
+        public string LiveOrderServiceBaseAddress {
+            get {
+                return ((string)(this["LiveOrderServiceBaseAddress"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LiveOrderServiceApiKey {
+            get {
+                return ((string)(this["LiveOrderServiceApiKey"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool TestMode {
+            get {
+                return ((bool)(this["TestMode"]));
+            }
+            set {
+                this["TestMode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("/Services/Orders/V5/CreateOrder")]
+        public string TestOrderServiceRequestUri {
+            get {
+                return ((string)(this["TestOrderServiceRequestUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("/V5/CreateOrder")]
+        public string LiveOrderServiceRequestUri {
+            get {
+                return ((string)(this["LiveOrderServiceRequestUri"]));
             }
         }
     }
